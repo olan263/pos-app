32e1fb0b652fcfc8be3b5f62fe357387f9a8630a
Olan <olanrina9@gmail.com>
2026-01-13 20:15:51 +0700

    KK

--- api/index.php
+++ api/index.php
@@ -1,2 +1,7 @@
 <?php
+
+// Memaksa PHP untuk tidak menampilkan peringatan deprecated
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+ini_set('display_errors', '0');
+
 require __DIR__ . '/../public/index.php';
